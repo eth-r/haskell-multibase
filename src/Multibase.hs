0e484a57c7f90caa7dd4eb8f1c3580e903bb701f
Promethea Raschke <eth.raschke@liminal.ai>
2018-01-06 23:48:01 +0000

    use ADT for base

--- src/Multibase.hs
+++ src/Multibase.hs
@@ -1,4 +1,4 @@
-module Multibase (
+module Multibase ( Base(..),
                   appendPrefix,
                   removePrefix,
                   getPrefix,
@@ -11,34 +11,61 @@ import qualified Data.ByteString as BStr
 import qualified Data.Map as M
 import MultibaseEncode
 
+data Base
+  = BaseID
+  | Base2
+  | Base8
+  | Base10
+  | Base16
+  | Base32
+  | Base32z
+  | Base32hex
+  | Base58btc
+  | Base58flickr
+  | Base64
+  | Base64URL
+  deriving (Eq, Ord, Show)
 
-appendPrefix ::  String -> BStr.ByteString -> BStr.ByteString
-appendPrefix a b =  BStr.append (BStr.pack $ snd $  fst $ (multibases M.! a)) b 
+appendPrefix ::  Base -> BStr.ByteString -> BStr.ByteString
+appendPrefix a b =  BStr.append (BStr.pack $ snd $  fst $ (multibases M.! a)) b
 
 removePrefix :: BStr.ByteString -> BStr.ByteString
 removePrefix a = BStr.pack $ tail (BStr.unpack a)
 
 getPrefix :: BStr.ByteString -> [W.Word8]
 getPrefix a = [head $ BStr.unpack a]
-  
-multiEncode :: String -> BStr.ByteString -> Bool -> BStr.ByteString
+
+multiEncode :: Base -> BStr.ByteString -> Bool -> BStr.ByteString
 multiEncode a b c =  appendPrefix a (encodeByteString b ( snd (multibases M.! a)) (fst $ fst (multibases M.! a)) c)
 
 multiDecode :: BStr.ByteString -> BStr.ByteString
 multiDecode a = decodeByteString (removePrefix a) (snd (multibases M.! (baseNumMap M.! (getPrefix a)))) (fst $ fst (multibases M.! (baseNumMap M.! (getPrefix a))))  0
-  
-multibases :: M.Map  String ((BStr.ByteString , [W.Word8]), Integer)
-multibases = M.fromList [("base id" , (( word8Range ,   [0] ), 256 )) ,
-                         ("base 2",(( base2   , [48]) , 2)) ,
-                         ("base 8",(( base8   , [55]) , 8)) ,
-                         ("base 10",(( base10   , [57]) , 10)) ,    
-                         ("base 16",(( base16  , [102]) , 16)) ,
-                         ("base 32",(( base32  , [98]) , 32)), 
-                         ("base 32z",(( base32z  , [104]) , 32)), 
-                         ("base 32hex",(( base32hex  , [118]) , 32 )),
-                         ("base 58btc",(( base58btc  , [122]) , 58)) ,
-                         ("base 58flickr",(( base58flickr  , [91]) , 58 )) ,
-                         ("base 64",(( base64  , [109]) , 64)) ,
-                         ("base 64URL" , (( base64URL  , [117]) , 64)) ]
-                         
-baseNumMap = M.fromList [([0] , "base id") , ([48] , "base 2") , ([55] , "base 8") , ([57] , "base 10") ,( [102] , "base 16"), ([98] , "base 32" ), ([104] , "base 32z") ,( [118] , "base 32hex")  ,( [122] , "base 58btc" ),( [91] , "base 58flickr") , ([109] , "base 64") ,( [117] , "base 64URL")]
+
+multibases :: M.Map Base ((BStr.ByteString , [W.Word8]), Integer)
+multibases = M.fromList [ (BaseID , (( word8Range ,   [0] ), 256 ))
+                        , (Base2,(( base2   , [48]) , 2))
+                        , (Base8,(( base8   , [55]) , 8))
+                        , (Base10,(( base10   , [57]) , 10))
+                        , (Base16,(( base16  , [102]) , 16))
+                        , (Base32,(( base32  , [98]) , 32))
+                        , (Base32z,(( base32z  , [104]) , 32))
+                        , (Base32hex,(( base32hex  , [118]) , 32 ))
+                        , (Base58btc,(( base58btc  , [122]) , 58))
+                        , (Base58flickr,(( base58flickr  , [91]) , 58 ))
+                        , (Base64,(( base64  , [109]) , 64))
+                        , (Base64URL , (( base64URL  , [117]) , 64))
+                        ]
+
+baseNumMap = M.fromList [([0] , BaseID)
+                        , ([48] , Base2)
+                        , ([55] , Base8)
+                        , ([57] , Base10)
+                        ,( [102] , Base16)
+                        , ([98] , Base32 )
+                        , ([104] , Base32z)
+                        ,( [118] , Base32hex)
+                        ,( [122] , Base58btc )
+                        ,( [91] , Base58flickr)
+                        , ([109] , Base64)
+                        ,( [117] , Base64URL)
+                        ]
