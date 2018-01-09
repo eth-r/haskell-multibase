79cc36b0455ee761565b58b41a619c76dabccfe3
Promethea Raschke <eth.raschke@liminal.ai>
2018-01-09 20:01:16 +0000

    add customization option

--- src/Data/ByteString/Multibase.hs
+++ src/Data/ByteString/Multibase.hs
@@ -28,6 +28,8 @@ module Data.ByteString.Multibase
   , base64pad
   , base64url
   , base64urlpad
+  , standardBases
+  , decodeWith
   ) where
 
 import Data.Char
@@ -57,19 +59,30 @@ getPrefix bytes = Just $ BStr.head bytes
 encode :: Base -> ByteString -> ByteString
 encode base bytes = addPrefix base $ baseEncode base bytes
 
+{-|
+Decode with the standard set of bases
+-}
 decode :: ByteString -> Result ByteString
-decode bytes =
-  decode' prefix encoded
+decode = decodeWith standardBases
+
+{-|
+Use a specific set of bases to decode from
+
+Can be used to add custom encodings in your application
+-}
+decodeWith :: M.Map Char Base -> ByteString -> Result ByteString
+decodeWith bases bytes =
+  decodeWith' bases prefix encoded
   where
     prefix = getPrefix bytes
     encoded = removePrefix bytes
 
-decode' :: Maybe Char -> Maybe ByteString -> Result ByteString
-decode' (Just prefix) (Just encoded) = do
-  case M.lookup prefix prefixBases of
+decodeWith' :: M.Map Char Base -> Maybe Char -> Maybe ByteString -> Result ByteString
+decodeWith' bases (Just prefix) (Just encoded) = do
+  case M.lookup prefix bases of
     Nothing -> Left UnknownBase
     Just base -> baseDecode base encoded
-decode' _ _ = Left InputTooShort
+decodeWith' _ _ _ = Left InputTooShort
 
 identity, base2, base8, base10, base16, base16' :: Base
 base32, base32', base32pad, base32pad' :: Base
@@ -83,36 +96,42 @@ identity = Base '\000'
   Right
 
 -- | Base 2
+--
 -- Currently uses an immature and extremely slow implementation
 base2 = Base '0'
   encodeBase2
   decodeBase2
 
 -- | Base 8
+--
 -- Currently uses an immature and extremely slow implementation
 base8 = Base '7'
   encodeBase8
   decodeBase8
 
 -- | Base 10
+--
 -- Inherently suffers from O(n^2) time complexity
 base10 = Base '9'
   encodeBase10
   decodeBase10
 
 -- | Base 16 (lowercase)
+--
 -- Uses a mature high-performance implementation
 base16 = Base 'f'
   encodeBase16
   decodeBase16
 
 -- | Base 16 (uppercase)
+--
 -- Uses a mature high-performance implementation
 base16' = Base 'F'
   (upperCase . encodeBase16)
   decodeBase16
 
 -- | Base 32 (lowercase, unpadded)
+--
 -- Uses a mature high-performance implementation
 base32 = Base 'b'
   encodeBase32LowercaseNoPad
@@ -125,78 +144,93 @@ base32' = Base 'B'
   decodeBase32
 
 -- | Base 32 (lowercase, padded)
+--
 -- Uses a mature high-performance implementation
 base32pad = Base 'c'
   encodeBase32Lowercase
   decodeBase32
 
 -- | Base 32 (uppercase, padded)
+--
 -- Uses a mature high-performance implementation
 base32pad' = Base 'C'
   encodeBase32
   decodeBase32
 
 -- | Base 32hex (lowercase, unpadded)
+--
 -- Uses a mature high-performance implementation
 base32hex = Base 'v'
   encodeBase32hexLowercaseNoPad
   decodeBase32hex
 
 -- | Base 32hex (uppercase, unpadded)
+--
 -- Uses a mature high-performance implementation
 base32hex' = Base 'V'
   encodeBase32hexNoPad
   decodeBase32hex
 
 -- | Base 32hex (lowercase, padded)
+--
 -- Uses a mature high-performance implementation
 base32hexpad = Base 't'
   encodeBase32hexLowercase
   decodeBase32hex
 
 -- | Base 32hex (uppercase, padded)
+--
 -- Uses a mature high-performance implementation
 base32hexpad' = Base 'T'
   encodeBase32hex
   decodeBase32hex
 
 -- | z-base-32
+--
 -- Uses a mature high-performance implementation
 base32z = Base 'h'
   encodeBase32z
   decodeBase32z
 
 -- | Base 58 (Bitcoin encoding)
--- inherently suffers from O(n^2) time complexity
+--
+-- Inherently suffers from O(n^2) time complexity,
+-- not recommended for encoding long bytestrings
 base58btc = Base 'z'
   encodeBase58btc
   decodeBase58btc
 
 -- | Base 58 (Flickr encoding)
--- inherently suffers from O(n^2) time complexity
+--
+-- Inherently suffers from O(n^2) time complexity,
+-- not recommended for encoding long bytestrings
 base58flickr = Base 'Z'
   encodeBase58flickr
   decodeBase58flickr
 
 -- | Base 64 (unpadded)
+--
 -- Uses a mature high-performance implementation
 base64 = Base 'm'
   (unpad . encodeBase64)
   (decodeBase64 . repad 4)
 
 -- | Base 64 (padded)
+--
 -- Uses a mature high-performance implementation
 base64pad = Base 'M'
   encodeBase64
   decodeBase64
 
 -- | Base 64url (unpadded)
+--
 -- Uses a mature high-performance implementation
 base64url = Base 'u'
   (unpad . encodeBase64URL)
   (decodeBase64URL . repad 4)
 
 -- | Base 64url (padded)
+--
 -- Uses a mature high-performance implementation
 base64urlpad = Base 'U'
   encodeBase64URL
@@ -229,4 +263,7 @@ bases =
 getBasePrefix :: Base -> (Char, Base)
 getBasePrefix base = (basePrefix base, base)
 
-prefixBases = M.fromList $ map getBasePrefix bases
+
+-- | Map of prefix character to the corresponding bases,
+-- exported for adding custom bases to the encoding set
+standardBases = M.fromList $ map getBasePrefix bases
